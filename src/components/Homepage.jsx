553f459c11648d9ac060f767dd197ed3b471df0a
LostButton <71197188+LostButton@users.noreply.github.com>
2022-10-06 08:09:22 -0400

    fixing gif

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,7 +12,7 @@ import template5 from "../cert_templates/Template5.png";
 import template6 from "../cert_templates/Template6.png";
 import template7 from "../cert_templates/Template7.png";
 import template8 from "../cert_templates/Template8.png";
-import template9 from "../cert_templates/Template9.png";
+import template9 from "../cert_templates/Template9.gif";
 import ReactToPrint from 'react-to-print';
 
 export class ComponentToPrint extends React.PureComponent {
