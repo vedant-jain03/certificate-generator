964db25904025dc0e27a67795736d4f04556c2c5
Yashika Jotwani <yashikajotwani12@gmail.com>
2022-10-15 19:49:17 +0530

    Removed dependencies and updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
+import React from 'react'
 
-export default function App() {
-  return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#1b272b' }}>
-      <MDBContainer className='p-4'></MDBContainer>
+const Footer = () => {
+  const year = new Date().getFullYear();
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2022 Copyright:
-        <a className='text-white'>
-          Certificate Generator
-        </a>
-      </div>
-    </MDBFooter>
-  );
-}
+  return <footer>{`Copyright © Certificate Generator ${year}`}</footer>;
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './index.css';
 import App from './App';
 ReactDOM.render(
