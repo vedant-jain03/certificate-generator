1af8cec5b2d30be8cf53273377c95ad5df3e574c
Yashika Jotwani <yashikajotwani12@gmail.com>
2022-10-15 11:25:47 +0530

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
+
+export default function App() {
+  return (
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#1b272b' }}>
+      <MDBContainer className='p-4'></MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className='text-white'>
+          Certificate Generator
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
