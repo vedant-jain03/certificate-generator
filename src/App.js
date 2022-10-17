3d586d574114d7e22223b7f7f31d02befad21fa6
Gunali Rezqi Mauludi <hello.gunalirezqi@gmail.com>
2022-10-17 16:25:30 +0700

    Added new template 'Teamplate 10' and fixed bug 'template name conflict in html id selector'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { useReactToPrint } from 'react-to-print';
+// import { useReactToPrint } from 'react-to-print';
 import Homepage from "./components/Homepage"
 import Footer from './components/Footer';
 import firebase from "./firebase"
@@ -23,31 +23,31 @@ var uiConfig = {
 function App() {
   const [user, setuser] = useState(null);
   useEffect(() => {
-    const authObserver = firebase.auth().onAuthStateChanged((user) => {
-      setuser(user);
-    })
+    // const authObserver = firebase.auth().onAuthStateChanged((user) => {
+    //   setuser(user);
+    // })
   })
-  if (user) {
+  // if (user) {
     return (
       <div>
         <Homepage />
         <Footer />
       </div>
     )
-  }
-  else {
-    return (
-      <div id="maincontainer1">
-        <h1 className="head">Hash/Hub</h1>
-        <h1 className="head2">Certificate Generator</h1>
-        <div className="signin">
-          <h1>Sign In</h1>
-          <StyleFirebaseUi uiConfig={uiConfig} firebaseAuth={firebase.auth()} setuser={setuser} user={user} />
-        </div>
+  // }
+  // else {
+  //   return (
+  //     <div id="maincontainer1">
+  //       <h1 className="head">Hash/Hub</h1>
+  //       <h1 className="head2">Certificate Generator</h1>
+  //       <div className="signin">
+  //         <h1>Sign In</h1>
+  //         <StyleFirebaseUi uiConfig={uiConfig} firebaseAuth={firebase.auth()} setuser={setuser} user={user} />
+  //       </div>
 
-      </div>
-    )
-  }
+  //     </div>
+  //   )
+  // }
 }
 
 export default App;
